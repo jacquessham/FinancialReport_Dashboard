27c4c0a0c8f9ed45712d4951d582be5884797c35
Jacques Sham <jacquessham2015@gmail.com>
2024-06-01 12:15:47 -0700

    Add dropdown list for company choice

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,26 +3,32 @@ from dash import dcc, html
 from dash.dependencies import Input, Output, State
 from dashboard_structure import *
 from static_vis import *
+from income_statement import income_statement_figure
+
 
 """
 Load your data here
 """
 
 # Dash Set up
-tab_labels = ['income_statement','balance_sheet','cashflow_statement']
+companies = ['Google','Apple','Meta','General Electric',
+    'Bank of America','Nvidia']
 port = 8000
 app = dash.Dash()
-app = dash_layout(app, tab_labels)
+app = dash_layout(app, tab_labels, companies)
 
 
 # Display Income Statement
 @app.callback([Output('content','children')],
-    [Input('dashboard-tabs','value')])
-def display_income_statement(tab):
+    [Input('dashboard-tabs','value'), Input('company-choices','value')])
+def display_income_statement(tab, company):
+    # Income Statement
     if tab == tab_labels[0]:
-        return [html.Div(dcc.Graph(figure=static_linechart()))]
+        return [html.Div(dcc.Graph(figure=income_statement_figure('Google')))]
+    # Balance Sheet
     if tab == tab_labels[1]:
         return [html.Div(dcc.Graph(figure=static_scatterplot()))]
+    # Cashflow Statement
     return [html.Div(dcc.Graph(figure=static_heatmap()))]
     
 
--- dashboard/dashboard_structure.py
+++ dashboard/dashboard_structure.py
@@ -13,12 +13,28 @@ def create_tabs(tab_labels):
 	return tabs
 
 
-def dash_layout(app, tab_labels):
+def dash_layout(app, tab_labels, dropdown_choices):
 	app.layout = html.Div([
+			# Dashboard Top
+			html.Div([
+				html.H1(children='Financial Dashboard',
+					style={'text-align':'center'}),
+				html.P('Select a company:'),
+				dcc.Dropdown(id='company-choices',
+					options=dropdown_choices,
+					value=dropdown_choices[0],
+					style={'width':'70%','text-align':'left'}
+					),
+				html.P()
+				]),
+			# Tabs for choosing the consoldiated statements
 			dcc.Tabs(
 				id='dashboard-tabs',value=tab_labels[0],
 				children=create_tabs(tab_labels)
 			),
+			# Div to display Graph
 			html.Div(id='content')
 		])
 	return app
+
+tab_labels = ['income_statement','balance_sheet','cashflow_statement']
