fd705318981479a239f42b75c5b315508f992bd0
Jacques Sham <jacquessham2015@gmail.com>
2024-08-03 01:28:48 -0700

    Add dropdown list for period selection

--- dashboard/dashboard_structure.py
+++ dashboard/dashboard_structure.py
@@ -19,12 +19,24 @@ def dash_layout(app, tab_labels, dropdown_choices):
 			html.Div([
 				html.H1(children='Financial Dashboard',
 					style={'text-align':'center'}),
-				html.P('Select a company:'),
-				dcc.Dropdown(id='company-choices',
-					options=dropdown_choices,
-					value=dropdown_choices[0],
-					style={'width':'70%','text-align':'left'}
-					),
+				
+				html.Div([
+					html.P('Select a company:'),
+					dcc.Dropdown(id='company-choices',
+						options=dropdown_choices,
+						value=dropdown_choices[0],
+						style={'width':'70%','text-align':'left'}
+						)
+					], style={'width':'80%', 'display':'inline-block'}),
+				html.Div([
+					html.P('Select the Reporting Period:'),
+					dcc.Dropdown(id='period-choices',
+						options=['Coming soon...','Coming soon 2...'],
+						value='Coming soon...',
+						style={'width':'90%','text-align':'left',
+							'float':'center'
+						})
+					], style={'width':'18%', 'display':'inline-block'}),
 				html.P()
 				]),
 			# Tabs for choosing the consoldiated statements
