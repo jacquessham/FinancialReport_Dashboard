6b2319199d928af4c420db4bd865d7545f24a254
Jacques Sham <jacquessham2015@gmail.com>
2024-08-15 15:57:57 -0700

    Fix typo

--- dashboard/cashflow_statement.py
+++ dashboard/cashflow_statement.py
@@ -6,7 +6,7 @@ from data.googl.cashflow_googl import *
 # Function called by dashboard.py
 def cashflow_statement_figure(company, data):
 	if company == 'googl':
-		return cashflow_statemetn_GOOGL(data) # For testing, delete later
+		return cashflow_statement_GOOGL(data) # For testing, delete later
 		# return example.fig # return poc image, delete later
 		# return cashflow_statemetn_GOOGL()
 	return None
