a4259f8c82e5e661156e0496adfdc8f7b1ce6982
Jacques Sham <jacquessham2015@gmail.com>
2024-08-29 16:28:05 -0700

    Drafted AAPL income statement script
    - Added cost of sales (Previously missing)

--- dashboard/income_statement.py
+++ dashboard/income_statement.py
@@ -2,6 +2,7 @@ import pandas as pd
 import data.googl.poc_charts.income_googl_poc as example # For poc purpose, delete later
 # from income_googl import *
 from data.googl.income_googl import *
+from data.aapl.income_aapl import *
 
 
 # Function called by dashboard.py
@@ -9,4 +10,6 @@ def income_statement_figure(company, data):
 	# For Google
 	if company == 'googl':
 		return income_statement_GOOGL(data)
+	elif company == 'aapl':
+		return income_statement_AAPL(data)
 	return None
