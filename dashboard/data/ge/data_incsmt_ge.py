facd7db8836c3d25dec9a39eba9963e4ec307256
Jacques Sham <jacquessham2015@gmail.com>
2024-09-02 13:44:24 -0700

    Drafted Income Statement script

--- dashboard/data/ge/data_incsmt_ge.py
+++ dashboard/data/ge/data_incsmt_ge.py
@@ -30,104 +30,90 @@ def get_data(df):
 	nodes = get_nodes()
 	nodes_label = [k for k in nodes.keys()]
 
-	# Node 16-19 taken care later since depends on +/- numbers
-	# Hardcode the colors
-	nodes_colors = ['gray']*10 + ['red'] + ['green'] + ['red']*4 
+	# Hardcode the colors and take care the color in the loops
+	nodes_colors = ['gray']*19
 
-	"""
+
 	# Map each KPI with the preassigned index number in nodes_label
 	df_temp = {'Items': [k for k in nodes.keys()], 
 		'Node_num':[nodes[k] for k in nodes.keys()]}
 	df = pd.merge(df, pd.DataFrame(df_temp), on='Items', how='inner')
 
-	# Add with itemized revenue
-	## Google Advertising
-	ads_value = 0
+	# Revenue
+	## Add Itemized revenue
 	for i in range(3):
 		df_revenue = df[df['Node_num']==i]
 		curr_value = df_revenue['Value'].values[0]
-		links = add_node_to_link(links, i, 7, curr_value, 'lightgray')
-		ads_value += curr_value
-	links = add_node_to_link(links, 7, 8, ads_value , 'lightgray')
-	
-	## Google Service
-	curr_value = df[df['Node_num']==3]['Value'].values[0]
-	srv_value = ads_value + curr_value
-	links = add_node_to_link(links, 3, 8, curr_value, 'lightgray')
+		links = add_node_to_link(links, i, 13, curr_value, 'lightgray')
 
-	## Revenue less Other Revenue
-	### Service Revenue
-	links = add_node_to_link(links, 8, 9, srv_value, 'lightgray')
-	### Google Clouds
-	links = add_node_to_link(links, 4, 9,
-		df[df['Node_num']==4]['Value'].values[0],'lightgray')
-
-	## Other Revenue - Hedging Gain and Other Bets
-	for i in range(5,7):
-		df_revenue = df[df['Node_num']==i]
-		curr_value = df_revenue['Value'].values[0]
-		if curr_value >= 0:
-			links = add_node_to_link(links, i, 9, curr_value, 'lightgray')
-		else:
-			links = add_node_to_link(links, 9, i, -1*curr_value, 'red')
-
-	# Add itemized expense
-	## Income from Operation
-	inc_op = 0
-	### Other income, net
-	curr_value = df[df['Node_num']==16]['Value'].values[0] 
-	if curr_value < 0:
-		links = add_node_to_link(links, 11, 16, -1*curr_value, 'pink')
-		nodes_colors.append('red')
+	## Other Income (Loss)
+	df_revenue = df[df['Node_num']==12]
+	curr_value = df_revenue['Value'].values[0]
+	if curr_value >= 0:
+		links = add_node_to_link(links, 12, 13, curr_value, 'lightgreen')
 	else:
-		links = add_node_to_link(links, 16, 11, curr_value, 'lightgray')
-		nodes_colors.append('gray')
-	inc_op += -1*curr_value
-
-	### EBIT
-	ebit = 0
-	nodes_colors.append(None) # Placeholder for EBIT node
-	#### Income Tax
-	curr_value = df[df['Node_num']==18]['Value'].values[0] 
-	if curr_value >0:
-		links = add_node_to_link(links, 17, 18, curr_value, 'pink')
-		nodes_colors.append('red')
-	else:
-		links = add_node_to_link(links, 18, 17, curr_value, 'lightgreen')
-		nodes_colors.append('green')
-	ebit += curr_value
-
-	if ebit > 0:
-		nodes_colors[17] = 'green'
-	else:
-		nodes_colors[17] = 'red'
-
-
-	#### Net Income
-	curr_value = df[df['Node_num']==19]['Value'].values[0] 
-	if curr_value >0:
-		links = add_node_to_link(links, 17, 19, curr_value, 'lightgreen')
-		nodes_colors.append('green')
-
-	else:
-		links = add_node_to_link(links, 19, 17, curr_value, 'pink')
-		nodes_colors.append('red')
-	ebit += curr_value
-	links = add_node_to_link(links, 11, 17, ebit, 'lightgreen')
-
-	## Income from Operation
-	inc_op += ebit
-	links = add_node_to_link(links, 9, 11, inc_op, 'lightgreen')
+		links = add_node_to_link(links, 13, 12, -1*curr_value, 'red')
+		nodes_colors[12] = 'red'
 
 	## Cost and Expense
 	cost_expense = 0
-	for i in range(12, 16):
+	for i in range(3, 12):
 		df_revenue = df[df['Node_num']==i]
 		curr_value = df_revenue['Value'].values[0]
 		cost_expense += curr_value
-		links = add_node_to_link(links, 10, i, curr_value, 'pink')
-	links = add_node_to_link(links, 9, 10, cost_expense, 'pink')
-	"""
+		if curr_value > 0: 
+			nodes_colors[i] = 'red'
+			links = add_node_to_link(links, 14, i, curr_value, 'pink')
+		# Non-operating benefit cost (income) could be negative
+		else:
+			nodes_colors[i] = 'green'
+			links = add_node_to_link(links, i, 14, -1*curr_value, 'lightgreen')
+	links = add_node_to_link(links, 13, 14, cost_expense, 'pink')
+
+	### EBIT
+	ebit = 0
+	#### Income Tax
+	#### Income Tax Provision is negative in GE, each is different from others
+	curr_value = df[df['Node_num']==16]['Value'].values[0] 
+	if curr_value >0:
+		links = add_node_to_link(links, 16, 15, curr_value, 'lightgreen')
+		nodes_colors[16] = 'green'
+		ebit += curr_value
+	else:
+		links = add_node_to_link(links, 15, 16, -1*curr_value, 'pink')
+		nodes_colors[16] = 'red'
+		ebit -= curr_value
+	
+	#### Net Income
+	curr_value = df[df['Node_num']==18]['Value'].values[0] 
+	if curr_value >0:
+		links = add_node_to_link(links, 15, 18, curr_value, 'lightgreen')
+		nodes_colors[18] = 'green'
+	else:
+		links = add_node_to_link(links, 18, 15, curr_value, 'pink')
+		nodes_colors[18] = 'red'
+	ebit += curr_value
+
+
+	#### Earning from discontinued operations, net of taxes
+	#### Positive is link toward EBIT
+	curr_value = df[df['Node_num']==17]['Value'].values[0] 
+	if curr_value >0:
+		links = add_node_to_link(links, 17, 15, curr_value, 'lightgreen')
+		nodes_colors[17] = 'green'
+		ebit -= curr_value
+	else:
+		links = add_node_to_link(links, 15, 17, -1*curr_value, 'pink')
+		nodes_colors[17] = 'red'
+		ebit += curr_value
+
+	# Change EBIT node colour
+	if ebit > 0:
+		links = add_node_to_link(links, 13, 15, ebit, 'lightgreen')
+		nodes_colors[15] = 'green'
+	else:
+		links = add_node_to_link(links, 15, 13, -1*ebit, 'pink')
+		nodes_colors[15] = 'red'
 
 	# Return result
 	return nodes_label, nodes_colors, links
