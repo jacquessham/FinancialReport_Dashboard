475605c85dcdc35b62555c502e03954e99400b49
Jacques Sham <jacquessham2015@gmail.com>
2024-09-03 17:23:19 -0700

    Drafted cashflow statement scripts

--- dashboard/cashflow_statement.py
+++ dashboard/cashflow_statement.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import data.googl.poc_charts.cashflow_googl_poc as example  # For poc purpose, delete later
 from data.googl.cashflow_googl import *
+from data.bac.cashflow_bac import *
 
 
 # Function called by dashboard.py
 def cashflow_statement_figure(company, data):
 	if company == 'googl':
 		return cashflow_statement_GOOGL(data)
+	elif company == 'bac':
+		return cashflow_statement_BAC(data)
 	return None
